6618ec1c75106afee2549266183056f0755c83fa
Susanne Wunsch <github@uriah.heep.sax.de>
2012-07-20 10:05:20 +0200

    Changed handling of second address line of a customer to avoid a perl error if the field is absent

--- lib/Gnucash/Business/Customer.pm
+++ lib/Gnucash/Business/Customer.pm
@@ -26,7 +26,12 @@ sub fillInfos($) {
 	next unless ($cust->child("cust:guid")->value() eq $self->{guid});
 	$self->{info}{address}{name} = $cust->child("cust:addr")->child("addr:name")->value();
 	$self->{info}{address}{addr1} = $cust->child("cust:addr")->child("addr:addr1")->value();
-	$self->{info}{address}{addr2} = $cust->child("cust:addr")->child("addr:addr2")->value();
+	if ($cust->child("cust:addr")->child("addr:addr2")) {
+	  $self->{info}{address}{addr2} =
+	    $cust->child("cust:addr")->child("addr:addr2")->value();
+	} else {
+	  $self->{info}{address}{addr2} = undef;
+	};
 	if ($cust->child("cust:addr")->child("addr:addr3")) {
 	  $self->{info}{address}{addr3} =
 	    $cust->child("cust:addr")->child("addr:addr3")->value();
@@ -45,8 +50,10 @@ sub getAddr3($) { return $_[0]->{info}{address}{addr3}; }
 sub getAddress($) {
     my $text = "";
     $text .= getName($_[0])."\n";
-    $text .= getAddr1($_[0])."\n";
-    $text .= getAddr2($_[0]);
+    $text .= getAddr1($_[0]);
+    if (getAddr2($_[0])) {
+      $text .= "\n".getAddr2($_[0]);
+    };
     if (getAddr3($_[0])) {
       $text .= "\n".getAddr3($_[0]);
     };
