47fb45d0b2290b950b8b42545985e67d694f85a2
Susanne Wunsch <github@uriah.heep.sax.de>
2012-07-20 10:04:07 +0200

    Added third address line from Gnucash's customer fields if available

--- lib/Gnucash/Business/Customer.pm
+++ lib/Gnucash/Business/Customer.pm
@@ -27,6 +27,12 @@ sub fillInfos($) {
 	$self->{info}{address}{name} = $cust->child("cust:addr")->child("addr:name")->value();
 	$self->{info}{address}{addr1} = $cust->child("cust:addr")->child("addr:addr1")->value();
 	$self->{info}{address}{addr2} = $cust->child("cust:addr")->child("addr:addr2")->value();
+	if ($cust->child("cust:addr")->child("addr:addr3")) {
+	  $self->{info}{address}{addr3} =
+	    $cust->child("cust:addr")->child("addr:addr3")->value();
+	} else {
+	  $self->{info}{address}{addr3} = undef;
+	};
 	last;
     }
 }
@@ -34,12 +40,16 @@ sub fillInfos($) {
 sub getName($) { return $_[0]->{info}{address}{name}; }
 sub getAddr1($) { return $_[0]->{info}{address}{addr1}; }
 sub getAddr2($) { return $_[0]->{info}{address}{addr2}; }
+sub getAddr3($) { return $_[0]->{info}{address}{addr3}; }
 
 sub getAddress($) {
     my $text = "";
     $text .= getName($_[0])."\n";
     $text .= getAddr1($_[0])."\n";
     $text .= getAddr2($_[0]);
+    if (getAddr3($_[0])) {
+      $text .= "\n".getAddr3($_[0]);
+    };
     return $text;
 }
 
